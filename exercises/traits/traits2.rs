d3f98ea6b3113122f246fb80e22015654bb004ed
corgile <mashiro-e@qq.com>
2024-07-19 04:16:27 +0800

    update traits

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -4,17 +4,22 @@
 // implement this trait, consider for a moment what it means to 'append "Bar"'
 // to a vector of strings.
 //
-// No boiler plate code this time, you can do this!
+// No boilerplate code this time, you can do this!
 //
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
-// TODO: Implement trait `AppendBar` for a vector of strings.
+//  Implement trait `AppendBar` for a vector of strings.
+impl AppendBar for Vec<String> {
+    fn append_bar(mut self) -> Self {
+        self.push(String::from("Bar"));
+        self
+    }
+}
 
 #[cfg(test)]
 mod tests {
