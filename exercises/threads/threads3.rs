3d79155c5d7107d43c58cf6f8aaf4441e2ae6a7f
corgile <mashiro-e@qq.com>
2024-07-19 04:03:02 +0800

    update threads

--- exercises/threads/threads3.rs
+++ exercises/threads/threads3.rs
@@ -3,10 +3,9 @@
 // Execute `rustlings hint threads3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::sync::mpsc;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
@@ -28,13 +27,17 @@ impl Queue {
 
 fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
     let qc = Arc::new(q);
+    let tx = Arc::new(Mutex::new(tx));  // 将 tx 包装在 Arc 和 Mutex 中
+
     let qc1 = Arc::clone(&qc);
     let qc2 = Arc::clone(&qc);
+    let tx1 = Arc::clone(&tx);
+    let tx2 = Arc::clone(&tx);
 
     thread::spawn(move || {
         for val in &qc1.first_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx1.lock().unwrap().send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
@@ -42,7 +45,7 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
     thread::spawn(move || {
         for val in &qc2.second_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx2.lock().unwrap().send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
